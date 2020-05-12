cfdf3fa8959cfe23f58d667a59c987b3e27c6de2
Youngho Kim <44015667+rxYoungho@users.noreply.github.com>
2020-05-12 14:54:37 +0900

    Update digitGenerator.py

--- BruteForce/digitGenerator.py
+++ BruteForce/digitGenerator.py
@@ -1,11 +1,20 @@
 n = int(input())
-
-#from n - 1, keep going down and compare
-tmp = n-1
-
-# for i in range(len(str(tmp))):
-#     guess = tmp+tmp[0]
-
-guess = tmp + tmp[0] + tmp[1]
-
-print(guess)
+real_n = n
+lista = []
+for x in range(n):    
+    sum_of = 0
+    product = 0
+    tmp = []
+    tmp_n = str(real_n - 1)
+    # print(tmp_n)
+    for i in range(len(str(tmp_n))):
+        tmp.append(str(tmp_n[i]))
+    for j in range(len(tmp)):
+        sum_of += int(tmp[j])
+    product = int(tmp_n) + sum_of
+    real_n -= 1
+    # print(product)
+    if product == n:
+        lista.append(tmp_n)
+    
+print(lista[-1])
